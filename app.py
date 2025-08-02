7c398c0cb5419f51df0044ab8cfe14402c034440
Jhonny Lima <lima.jhonny95@gmail.com>
2025-08-02 16:19:16 -0300

    feat: add `logout` route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from database import db
 from models.user import User
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -32,6 +32,12 @@ def login():
   login_user(user)
   print(current_user.is_authenticated)
   return jsonify({"message": "Login successful"})
+
+@app.route('/logout', methods=['POST'])
+@login_required
+def logout():
+  logout_user()
+  return jsonify({"message": "Logged out successfully"})
 
 if __name__ == '__main__':
   app.run(debug=True)
